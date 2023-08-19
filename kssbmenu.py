496c24cd97ebc097e9697db07a38cf50ed437c2c
Quin <quin.marilyn05@gmail.com>
2023-08-19 14:03:53 -0600

    Chore: Improve KSSB Menu's code considerably.

--- kssbmenu.py
+++ kssbmenu.py
@@ -1,26 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-class kssb_menu():
-	def __init__(self):
-		self.url = "https://kssb.net/parents/menus/"
-		self.session = requests.session()
-	
-	def download(self):
-		r = self.session.get(self.url)
-		soup = BeautifulSoup(r.text, features="html.parser")
-		days = []
-		menus = []
-		menu = {}
-		for t in soup.find_all("div", class_="vc-hoverbox-front-inner"):
-			days.append(t.text[1:-1])
-		for tag in soup.find_all("div", class_="vc-hoverbox-back-inner"):
-			temp = []
-			for p in tag.find_all("p"):
-				temp.append(p.text)
-			menus.append('\n'.join(temp))
-		
-		for i in range(len(days)):
-			menu[days[i]] = menus[i]
-		
-		return menu
+
+class KssbMenu:
+    def __init__(self):
+        self.url = "https://kssb.net/parents/menus/"
+        self.session = requests.session()
+
+    def download(self):
+        response = self.session.get(self.url)
+        soup = BeautifulSoup(response.text, features="html.parser")
+
+        menu = {}
+        for front, back in zip(
+            soup.find_all("div", class_="vc-hoverbox-front-inner"),
+            soup.find_all("div", class_="vc-hoverbox-back-inner"),
+        ):
+            day = front.text[1:-1]
+            menu[day] = "\n".join([p.text for p in back.find_all("p")])
+
+        return menu
