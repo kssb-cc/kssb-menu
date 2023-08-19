e903b176633520b2a870a84bdd629a574f4870ae
Quin <quin.marilyn05@gmail.com>
2023-08-19 14:04:25 -0600

    Minor formatting.

--- kssbmenu.py
+++ kssbmenu.py
@@ -10,7 +10,6 @@ class KssbMenu:
     def download(self):
         response = self.session.get(self.url)
         soup = BeautifulSoup(response.text, features="html.parser")
-
         menu = {}
         for front, back in zip(
             soup.find_all("div", class_="vc-hoverbox-front-inner"),
@@ -18,5 +17,4 @@ class KssbMenu:
         ):
             day = front.text[1:-1]
             menu[day] = "\n".join([p.text for p in back.find_all("p")])
-
         return menu
